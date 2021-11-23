13ed56f8fcb88f53c76ba2484af0f0da25d4851e
AYUSHMAN-PANDA <panda.ayushman@gmail.com>
2021-11-24 03:01:44 +0530

    added title bar

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import "./Slider.css";
 import BtnSlider from "./BtnSlider";
 import dataSlider from "./dataSlider";
+import { Navbar, Container, Image } from "react-bootstrap";
+import logo from "../../images/Nav-logo.png";
 
 export default function Slider() {
   const [slideIndex, setSlideIndex] = useState(1);
@@ -27,38 +29,64 @@ export default function Slider() {
   };
 
   return (
-    <div className="container-slider">
-      {dataSlider.map((obj, index) => {
-        return (
-          <div
-            id={obj.id}
-            key={obj.id}
-            className={slideIndex === index + 1 ? "slide active-anim" : "slide"}
-          >
-            <a
-              href={dataSlider[slideIndex - 1].result}
-              rel="noreferrer"
-              target="_blank"
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <a style={{ textDecoration: "none" }} href="privateprivates.ml">
+            <Navbar.Brand
+              style={{
+                textAlign: "center",
+                justifyContent: "center",
+                color: "#f4abba",
+              }}
             >
-              <img
-                alt={`img${index + 1}.jpg`}
-                src={process.env.PUBLIC_URL + `/Imgs/img${index + 1}.jpg`}
+              <Image
+                alt=""
+                src={logo}
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
               />
-            </a>
-          </div>
-        );
-      })}
-      <BtnSlider moveSlide={nextSlide} direction={"next"} />
-      <BtnSlider moveSlide={prevSlide} direction={"prev"} />
+              Where you at ?
+            </Navbar.Brand>
+          </a>
+        </Container>
+      </Navbar>
+      <div className="container-slider">
+        {dataSlider.map((obj, index) => {
+          return (
+            <div
+              id={obj.id}
+              key={obj.id}
+              className={
+                slideIndex === index + 1 ? "slide active-anim" : "slide"
+              }
+            >
+              <a
+                href={dataSlider[slideIndex - 1].result}
+                rel="noreferrer"
+                target="_blank"
+              >
+                <img
+                  alt={`img${index + 1}.jpg`}
+                  src={process.env.PUBLIC_URL + `/Imgs/img${index + 1}.jpg`}
+                />
+              </a>
+            </div>
+          );
+        })}
+        <BtnSlider moveSlide={nextSlide} direction={"next"} />
+        <BtnSlider moveSlide={prevSlide} direction={"prev"} />
 
-      <div className="container-dots">
-        {Array.from({ length: 3 }).map((item, index) => (
-          <div
-            onClick={() => moveDot(index + 1)}
-            className={slideIndex === index + 1 ? "dot active" : "dot"}
-          ></div>
-        ))}
+        <div className="container-dots">
+          {Array.from({ length: 3 }).map((item, index) => (
+            <div
+              onClick={() => moveDot(index + 1)}
+              className={slideIndex === index + 1 ? "dot active" : "dot"}
+            ></div>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
