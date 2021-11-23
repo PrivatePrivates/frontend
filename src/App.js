1a5bcbcce5f9600ab37f3b6d404c3ec56151d4d6
vjspranav <pranavasri@live.in>
2021-11-24 02:21:16 +0530

    Add a route to old dashboard

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Deck from "./components/Slider/Slider";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import { HashRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Router>
         <Route exact path="/" component={Deck} />
+        <Route path="/old" component={Dashboard} />
       </Router>
     </>
   );
