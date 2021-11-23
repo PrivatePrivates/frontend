11cd140fb8a4074470afec79cd40579a582597ba
AYUSHMAN-PANDA <panda.ayushman@gmail.com>
2021-11-24 02:09:16 +0530

    added images

--- src/components/Slider/dataSlider.js
+++ src/components/Slider/dataSlider.js
@@ -3,20 +3,17 @@ import { v4 as uuidv4 } from "uuid";
 const dataSlider = [
   {
     id: uuidv4(),
-    title: "Lorem ipsum",
-    subTitle: "Lorem",
+    title: "Victim 1",
     result: "http://example.privateprivates.ml/victim-network-1.html",
   },
   {
     id: uuidv4(),
-    title: "Lorem ipsum",
-    subTitle: "Lorem",
+    title: "Victim 2",
     result: "http://example.privateprivates.ml/victim-network-2.html",
   },
   {
     id: uuidv4(),
-    title: "Lorem ipsum",
-    subTitle: "Lorem",
+    title: "Victim 3",
     result: "http://example.privateprivates.ml/victim-network-3.html",
   },
 ];
